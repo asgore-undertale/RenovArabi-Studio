29299b34d924262c0aa8c054c48d525cdb2ff11b
asgore-undertale <52295461+asgore-undertale@users.noreply.github.com>
2021-11-24 20:14:43 +0200

    test

---
+++ RenovArabi-Studio/Program.cs
@@ -0,0 +1,58 @@
+﻿int i;
+i = test(6);
+Console.WriteLine("Hello, World! " + i);
+Console.ReadKey();
+
+static int test(int i)
+{
+    return i;
+}
+
+if (i == 6) {}
+else {}
+
+switch (i) {
+    case 0:
+        break;
+    default:
+        break;
+}
+
+int[] vs = new int[6];
+int[] vs2 = new int[] {1, 2, 3, 4, 5, 6};
+
+List<int> list = new List<int>() {1, 2, 3};
+list.Add(3);
+list.Remove(1);
+
+foreach (int j in vs) {}
+
+for (int i = 0; i < vs2.Length; i++) {}
+
+class testClass {}
+
+class MyClass : testClass {
+    public MyClass() {
+        // constructor
+    }
+
+    private void Test() {}
+}
+
+MyClass myClass = new MyClass();
+
+/*
+using System;
+
+namespace RenovArabi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
+*/
